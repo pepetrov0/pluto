e3228aa865cffbfb588366fdd4303919689f987c
Petar Petrov <ppetrov@pbyte.xyz>
2024-01-15 18:39:31 +0200

    basic tracing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use axum::{routing, Router};
+use tower_http::trace::TraceLayer;
 
 mod ping;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/api/ping", routing::get(ping::handler));
+    // initialize tracing
+    tracing_subscriber::fmt()
+        .compact()
+        .with_max_level(tracing::Level::TRACE)
+        .init();
+
+    let app = Router::new()
+        .route("/api/ping", routing::get(ping::handler))
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
