7b4b6ad435db2cefadee21c74d49a00d22a54814
Petar Petrov <ppetrov@pbyte.xyz>
2024-04-17 22:47:10 +0300

    tracing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use pluto::{domain::configuration::Configuration, web};
 use tokio::net::TcpListener;
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
-    let config = Configuration::try_load().unwrap_or_default();
-    println!("cfg: {config:?}");
+    #[cfg(debug_assertions)]
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_max_level(Level::DEBUG)
+        .init();
+
+    #[cfg(not(debug_assertions))]
+    tracing_subscriber::fmt()
+        .compact()
+        .with_max_level(Level::INFO)
+        .init();
+
+    let _config = Configuration::try_load().unwrap_or_default();
     let router = web::router();
     let listener = TcpListener::bind("0.0.0.0:46963").await.unwrap();
+
+    tracing::info!("listening on 0.0.0.0:46963");
     axum::serve(listener, router).await.unwrap();
+    tracing::info!("shutting down..");
 }
