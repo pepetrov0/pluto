8242192511d67e32b4e8578bfb66f4e9b8f29705
Petar Petrov <ppetrov@pbyte.xyz>
2024-04-25 19:06:11 +0300

    compression layer + health

--- pretest.sh
+++ pretest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "PLUTO__DATABASE=postgresql://local:local@localhost/local" > .env
 docker compose -f local.compose.yaml up --force-recreate -dV
+rm -rf database.db database.db-wal database.db-shm
 
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,10 +1,13 @@
 //! This module implements the web interface of the application.
 
-use axum::Router;
+use axum::{routing, Router};
+use tower_http::compression::CompressionLayer;
 
 /// Constructs the primary router to be used for serving the application.
 #[tracing::instrument]
 pub fn router() -> Router {
     tracing::debug!("constructing router..");
     Router::new()
+        .route("/health", routing::get(()))
+        .layer(CompressionLayer::new())
 }
