9f89ce116c094ca3ae3f19443d3f90dbce4199fb
Petar Petrov <ppetrov@pbyte.xyz>
2024-05-18 17:46:49 +0300

    root level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,23 @@
 //! pluto is a personal finance management application
+//!
+//! --- STRUCTURE ---
+//! pluto follows Action-Domain-Response pattern where the domain logic is 
+//! separated from the the representation logic and thus the core is 
+//! separated into two modules: domain and web.
+//!
+//! The domain logic lives in the `domain` module and is further divided
+//! by features and/or layer.
+//!
+//! The presentation logic lives in the `web` module and is further divided 
+//! into features - which in turn are divided into action and responder pair.
+//!
 
 #![deny(unused)]
 
 pub mod domain;
 pub mod web;
 
-const NAME: &str = env!("CARGO_PKG_NAME");
+pub const NAME: &str = env!("CARGO_PKG_NAME");
 
-// init i18n
+// This intializes the locales.
 rust_i18n::i18n!("locales");
