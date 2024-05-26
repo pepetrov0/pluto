d9b11a66f2107eb7909f6541a322c47c317c9653
Petar Petrov <ppetrov@pbyte.xyz>
2024-05-26 17:42:03 +0300

    gaps

--- src/web/_components/organisms/change_email_form.rs
+++ src/web/_components/organisms/change_email_form.rs
@@ -15,11 +15,11 @@ pub struct ChangeEmailFormData {
 /// A form that allows the user to change their email.
 pub fn change_email_form(locale: &str, data: Option<ChangeEmailFormData>) -> Markup {
     const STYLES: &str = "card grid gap-2 grid-cols-1 sm:grid-cols-2";
-    const TITLE_STYLES: &str = "sm:col-span-2 flex flex-row gap-2 items-center mb-2";
+    const TITLE_STYLES: &str = "sm:col-span-2 flex flex-row gap-2 items-center mb-4";
     // const ERROR_LABEL_STYLES: &str = "text-sm text-red-500";
     const FIELD_CONTAINER_STYLES: &str = "w-full flex flex-col gap-1";
     const FIELD_LABEL_STYLES: &str = "flex flex-row gap-2 items-center";
-    const ACTIONS_STYLES: &str = "sm:col-span-2 flex flex-row items-center justify-end mt-2";
+    const ACTIONS_STYLES: &str = "sm:col-span-2 flex flex-row items-center justify-end mt-4";
     const ACTION_STYLES: &str = "sm:w-fit";
 
     let data = data.unwrap_or_default();
